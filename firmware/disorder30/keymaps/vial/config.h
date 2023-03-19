b83b3a729ca8b09e1751df3e77c33cc57eb77787
Mikko Kylämä <mikko.kylama@gmail.com>
2023-03-19 18:05:55 +0200

    Add missing config.h for vial keymap

---
+++ firmware/disorder30/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x9F, 0x5E, 0x7A, 0xE2, 0xB1, 0x3E, 0x7A, 0x56}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 7 }
